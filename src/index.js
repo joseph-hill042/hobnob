196c49957680d90ae24020c9ab9ac8ed6298ee89
Joseph Hill <joseph.hill1@outlook.com>
2018-11-11 21:56:20 -0500

    Added more test case scenarios and refactored to DRY up code by using regex.

--- src/index.js
+++ src/index.js
@@ -3,16 +3,47 @@ import '@babel/polyfill'
 import * as http from 'http'
 const requestHandler = function(req, res) {
   if (req.method === 'POST' && req.url === '/users') {
-    res.writeHead(400, { 'Content-Type': 'application/json' })
-    res.end(
-      JSON.stringify({
-        message: 'Payload should not be empty',
-      })
-    )
-    return
+    const payloadData = []
+    req.on('data', data => {
+      payloadData.push(data)
+    })
+
+    req.on('end', () => {
+      if (payloadData.length === 0) {
+        res.writeHead(400, { 'Content-Type': 'application/json' })
+        res.end(
+          JSON.stringify({
+            message: 'Payload should not be empty',
+          })
+        )
+        return
+      }
+      if (req.headers['content-type'] !== 'application/json') {
+        res.writeHead(415, { 'Content-Type': 'application/json' })
+        res.end(
+          JSON.stringify({
+            message:
+              'The "Content-Type" header must always be "application/json"',
+          })
+        )
+        return
+      }
+      try {
+        const bodyString = Buffer.concat(payloadData).toString()
+        JSON.parse(bodyString)
+      } catch (e) {
+        res.writeHead(400, { 'Content-Type': 'application/json' })
+        res.end(
+          JSON.stringify({
+            message: 'Payload should be in JSON format',
+          })
+        )
+      }
+    })
+  } else {
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.end('Hello, World!')
   }
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello, World!')
 }
 const server = http.createServer(requestHandler)
 server.listen(process.env.SERVER_PORT)
