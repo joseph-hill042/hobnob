2758cc22a54af569c62d096fe896c42701747d5f
Joseph Hill <joseph.hill1@outlook.com>
2018-11-11 19:42:43 -0500

    Implemented test suite and environment variables.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+import './env'
 import '@babel/polyfill'
 import * as http from 'http'
 const requestHandler = function(req, res) {
+  if (req.method === 'POST' && req.url === '/users') {
+    res.writeHead(400, { 'Content-Type': 'application/json' })
+    res.end(
+      JSON.stringify({
+        message: 'Payload should not be empty',
+      })
+    )
+    return
+  }
   res.writeHead(200, { 'Content-Type': 'text/plain' })
   res.end('Hello, World!')
 }
 const server = http.createServer(requestHandler)
-server.listen(8080)
+server.listen(process.env.SERVER_PORT)
